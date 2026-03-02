87a7b843a51d90d974ed9a0fda8539d40a6a6ca7
Olyad Negero <olyadnegero@gmail.com>
2026-03-02 18:07:55 +0300

    The name of component 'navbar' is changed to 'socials' to better represent the content

--- components/Socials.tsx
+++ components/Socials.tsx
@@ -3,12 +3,12 @@ import Link from "next/link";
 
 function Footer() {
   return (
-    <footer className="links-container flex gap-8 text-(--accent-1) text-lg capitalize p-4">
+    <div className="links-container flex gap-8 text-(--accent-1) text-lg capitalize p-4">
       <Link href="#">contaxt</Link>
       <Link href="#">social</Link>
       <Link href="#">address</Link>
       <Link href="#">legal terms</Link>
-    </footer>
+    </div>
   );
 }
 
