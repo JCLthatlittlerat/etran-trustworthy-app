4053462e3a02c41c27746d7ac07cf8d7a58c2a5c
Olyad Negero <olyadnegero@gmail.com>
2026-03-02 17:47:32 +0300

    The mock data, that is intended to represent data from API, is modified to add the second card's data

--- components/Card.tsx
+++ components/Card.tsx
@@ -2,7 +2,7 @@ import { data } from "@/store/data";
 
 export default function Card() {
   return (
-    <div className=" grid grid-cols-3 py-10 gap-4">
+    <div className=" grid grid-cols-3 py-10 gap-4 ">
       {data.card.map((item) => {
         const Icon = item.icon;
         return (
@@ -10,8 +10,8 @@ export default function Card() {
             key={item.id}
             className="card bg-(--accent-3) rounded-lg p-10 flex flex-col items-center gap-4"
           >
-            <Icon size={80} />
-            <h2 className="text-xl capitalize max-w-[10ch] text-center">
+            <Icon size={70} />
+            <h2 className="text-2xl capitalize max-w-[10ch] text-center ">
               {item.title}
             </h2>
           </div>
--- store/data.ts
+++ store/data.ts
@@ -4,7 +4,8 @@ import { RxGlobe } from "react-icons/rx";
 import { GoShieldCheck } from "react-icons/go";
 
 export const data: {
-  card: { id: number; title: string; icon: IconType }[];
+  card: { id: number; title: string; icon: IconType | string }[];
+  card2: { id: number; title: string; icon: string }[];
 } = {
   card: [
     {
@@ -21,6 +22,28 @@ export const data: {
       id: 3,
       title: "advanced technology",
       icon: GoShieldCheck,
+    },
+  ],
+  card2: [
+    {
+      id: 1,
+      title: "double your productivity",
+      icon: `2x`,
+    },
+    {
+      id: 2,
+      title: "efficiency increase per transfer",
+      icon: `/increasing-efficiency-graph.svg`,
+    },
+    {
+      id: 3,
+      title: "centralize your finance",
+      icon: `/centralize-your-finances.svg`,
+    },
+    {
+      id: 4,
+      title: "more activity",
+      icon: `100%`,
     },
   ],
 };
